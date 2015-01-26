599c7ee54d0f115acf8caf658f5cc3fb6dc6dc7e
MrCraftCod <zerderr@gmail.com>
2015-01-26 23:25:46 +0100

    Commands.java javadoc

--- src/fr/tours/polytech/DI/RFID/enums/Commands.java
+++ src/fr/tours/polytech/DI/RFID/enums/Commands.java
@@ -2,6 +2,11 @@ package fr.tours.polytech.DI.RFID.enums;
 
 import fr.tours.polytech.DI.RFID.utils.Utils;
 
+/**
+ * Enumeration for the commands that can be sent to the contactless card.
+ *
+ * @author COLEAU Victor, COUCHOUD Thomas
+ */
 public enum Commands
 {
 	UID("UID", new byte[]
@@ -9,22 +14,41 @@ public enum Commands
 	private byte[] command;
 	private String name;
 
+	/**
+	 * Constructor.
+	 *
+	 * @param name The name of the command.
+	 * @param command The command.
+	 */
 	Commands(String name, byte[] command)
 	{
 		this.command = command;
 		this.name = name;
 	}
 
+	/**
+	 * Used to get the command.
+	 *
+	 * @return The command.
+	 */
 	public byte[] getCommand()
 	{
 		return this.command;
 	}
 
+	/**
+	 * Used to get the name.
+	 *
+	 * @return The name.
+	 */
 	public String getName()
 	{
 		return this.name;
 	}
 
+	/**
+	 * Used to get a String representing this item, will be formatted as <b>[name] ([command])</b>
+	 */
 	@Override
 	public String toString()
 	{

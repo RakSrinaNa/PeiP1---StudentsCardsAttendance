efd040ff01e87862266a59597d5e3914b791ae4a
MrCraftCod <zerderr@gmail.com>
2015-01-26 19:01:19 +0100

    Period.java javadoc

--- src/fr/tours/polytech/DI/RFID/objects/Period.java
+++ src/fr/tours/polytech/DI/RFID/objects/Period.java
@@ -9,6 +9,11 @@ import java.util.regex.Pattern;
 import fr.tours.polytech.DI.RFID.utils.Configuration;
 import fr.tours.polytech.DI.RFID.utils.Utils;
 
+/**
+ * Class representing a period for checking.
+ *
+ * @author COLEAU Victor, COUCHOUD Thomas
+ */
 public class Period
 {
 	private int startingHour;
@@ -18,6 +23,13 @@ public class Period
 	private Calendar calendar;
 	private DecimalFormat decimalFormat;
 
+	/**
+	 * Constructor.
+	 *
+	 * @param period A string representing the period. This should be formatted as <i>xx</i><b>h</b><i>xx</i><b>-</b><i>yy</i><b>h</b><i>yy</i> where <i>xx</i> and <i>yy</i> are the time to set.
+	 *
+	 * @throws IllegalArgumentException If the period isn't formatted as it should be.
+	 */
 	public Period(String period) throws IllegalArgumentException
 	{
 		if(!Pattern.matches("(\\d{1,2})(h|H)(\\d{1,2})(-)(\\d{1,2})(h|H)(\\d{1,2})", period))
@@ -33,6 +45,11 @@ public class Period
 		this.decimalFormat = new DecimalFormat("00");
 	}
 
+	/**
+	 * Used to get all the periods from the config file.
+	 *
+	 * @return An array list of the periods.
+	 */
 	public static ArrayList<Period> loadPeriods()
 	{
 		ArrayList<Period> periods = new ArrayList<Period>();
@@ -42,18 +59,29 @@ public class Period
 			{
 				periods.add(new Period(stringPeriod));
 			}
-			catch(Exception e)
+			catch(Exception exception)
 			{
-				Utils.logger.log(Level.WARNING, "Can't load perriod " + stringPeriod, e);
+				Utils.logger.log(Level.WARNING, "Can't load perriod " + stringPeriod, exception);
 			}
 		return periods;
 	}
 
+	/**
+	 * Used to get a String representing this interval.
+	 *
+	 * @return A string formatted as <b>xxHxx - yyHyy</b>
+	 */
 	public String getTimeInterval()
 	{
 		return this.startingHour + "H" + this.decimalFormat.format(this.startingMinute) + " - " + this.endingHour + "H" + this.decimalFormat.format(this.endingMinute);
 	}
 
+	/**
+	 * Used to know if the date is in this period.
+	 *
+	 * @param date The date to verify.
+	 * @return true if the date is in the period, false if not.
+	 */
 	public boolean isInPeriod(Date date)
 	{
 		this.calendar.setTime(date);
@@ -81,7 +109,13 @@ public class Period
 		return false;
 	}
 
-	public boolean isOverlapped(Period period) // TODO
+	/**
+	 * Used to know if two Period objects are overlapping.
+	 *
+	 * @param period The other Period to check with.
+	 * @return true if overlapping, false if not.
+	 */
+	public boolean isOverlapped(Period period)
 	{
 		if(period == null)
 			return false;
@@ -90,12 +124,22 @@ public class Period
 		return false;
 	}
 
+	/**
+	 * Used to get a String representing this interval. This is mostly used when saving the object to the config, should use {@link getTimeInterval} instead.
+	 *
+	 * @return A string formatted as <b>xxHxx-yyHyy</b>
+	 */
 	@Override
 	public String toString()
 	{
 		return getTimeInterval().replaceAll(" ", "");
 	}
 
+	/**
+	 * Use to get the ending date of this period in the current day.
+	 *
+	 * @return The ending date.
+	 */
 	private Date getEndingDate()
 	{
 		Calendar calen = Calendar.getInstance();
@@ -105,6 +149,11 @@ public class Period
 		return calen.getTime();
 	}
 
+	/**
+	 * Use to get the starting date of this period in the current day.
+	 *
+	 * @return The starting date.
+	 */
 	private Date getStartingDate()
 	{
 		Calendar calen = Calendar.getInstance();

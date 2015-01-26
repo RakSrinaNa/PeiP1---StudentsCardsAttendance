38a612cfd43d8fefa0b8c3f951b6b36f6779952f
MrCraftCod <zerderr@gmail.com>
2015-01-26 18:07:14 +0100

    Main.java javadoc

--- src/fr/tours/polytech/DI/RFID/Main.java
+++ src/fr/tours/polytech/DI/RFID/Main.java
@@ -1,12 +1,28 @@
 package fr.tours.polytech.DI.RFID;
 
 import java.io.IOException;
-import java.util.NoSuchElementException;
 import fr.tours.polytech.DI.RFID.utils.Utils;
 
+/**
+ * Program used to replace the attendance sheet with the student cards.
+ *
+ * @author COLEAU Victor, COUCHOUD Thomas
+ * @version 1.0
+ * @since 1.8
+ */
 public class Main
 {
-	public static void main(String[] args) throws NoSuchElementException, SecurityException, IOException
+	/**
+	 * The main function, launched on startup.
+	 *
+	 * @param args Arguments for the program - Not used.
+	 *
+	 * @throws SecurityException If the Student.csv file can't be read.
+	 * @throws IOException If the Student.csv file can't be read.
+	 *
+	 * @see java.util.logging.FileHandler.FileHandler#FileHandler(String, boolean)
+	 */
+	public static void main(String[] args) throws SecurityException, IOException
 	{
 		Utils.init();
 	}

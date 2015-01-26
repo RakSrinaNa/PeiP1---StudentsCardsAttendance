3a837d6dfcbc5afab6865f1dd2c8ff7e04e7025a
MrCraftCod <zerderr@gmail.com>
2015-01-26 23:39:01 +0100

    JTableUneditableModel.java javadoc

--- src/fr/tours/polytech/DI/RFID/frames/components/JTableUneditableModel.java
+++ src/fr/tours/polytech/DI/RFID/frames/components/JTableUneditableModel.java
@@ -2,15 +2,34 @@ package fr.tours.polytech.DI.RFID.frames.components;
 
 import javax.swing.table.DefaultTableModel;
 
+/**
+ * Used to create an uneditable JTable.
+ *
+ * @author COLEAU Victor, COUCHOUD Thomas
+ */
 public class JTableUneditableModel extends DefaultTableModel
 {
 	private static final long serialVersionUID = 1595933236184593763L;
 
-	public JTableUneditableModel(Object[][] tableData, Object[] colNames)
+	/**
+	 * Constructor.
+	 *
+	 * @param tableData The data of the table.
+	 * @param columnsNames The columns name.
+	 *
+	 * @see DefaultTableModel#DefaultTableModel(Object[][], Object[])
+	 */
+	public JTableUneditableModel(Object[][] tableData, Object[] columnsNames)
 	{
-		super(tableData, colNames);
+		super(tableData, columnsNames);
 	}
 
+	/**
+	 * Used to know if the cell id editable.
+	 *
+	 * @return false as we want the table to be uneditable.
+	 */
+	@Override
 	public boolean isCellEditable(int row, int column)
 	{
 		return false;

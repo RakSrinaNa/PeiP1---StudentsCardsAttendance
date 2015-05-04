bea9aae0a5489631880a3473ca74ebaa10d5a203
MrCraftCod <zerderr@gmail.com>
2015-05-04 12:43:46 +0200

    Reformat for Maven

---
+++ src/main/java/polytech/DI/RFID/Main.java
@@ -0,0 +1,26 @@
+package polytech.DI.RFID;
+
+import polytech.DI.RFID.utils.Utils;
+import java.io.IOException;
+
+/**
+ * Program used to replace the attendance sheet with the student cards.
+ *
+ * @author COLEAU Victor, COUCHOUD Thomas
+ * @version 1.0
+ */
+public class Main
+{
+	/**
+	 * The main function, launched on startup.
+	 *
+	 * @param args Arguments for the program - Not used.
+	 * @throws IOException If files couldn't be read.
+	 * @throws SecurityException If the database connection can't be made.
+	 * @see Utils#init(String[])
+	 */
+	public static void main(String[] args) throws SecurityException, IOException
+	{
+		Utils.init(args);
+	}
+}
---
+++ src/main/java/polytech/DI/RFID/frames/components/StudentsRenderer.java
@@ -0,0 +1,52 @@
+package polytech.DI.RFID.frames.components;
+
+import polytech.DI.RFID.objects.Student;
+import polytech.DI.RFID.utils.Utils;
+import javax.swing.*;
+import javax.swing.table.TableCellRenderer;
+import java.awt.*;
+
+/**
+ * Renderer for the students table.
+ *
+ * @author COLEAU Victor, COUCHOUD Thomas
+ */
+public class StudentsRenderer implements TableCellRenderer
+{
+	private final TableCellRenderer wrappedRenderer;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param wrappedRenderer The default renderer wrapped to the table.
+	 */
+	public StudentsRenderer(TableCellRenderer wrappedRenderer)
+	{
+		this.wrappedRenderer = wrappedRenderer;
+	}
+
+	/**
+	 * Used to get the colour of the cell.
+	 *
+	 * @param value The value of the cell.
+	 * @return The color to set for this student.
+	 */
+	public Color getTableBackgroundColour(Student value)
+	{
+		return Utils.hasChecked(value) ? Color.GREEN : Color.ORANGE;
+	}
+
+	/**
+	 * Set the component drawn to have the background set to the correct colour.
+	 *
+	 * @see TableCellRenderer#getTableCellRendererComponent(JTable, Object, boolean, boolean, int, int)
+	 */
+	@Override
+	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column)
+	{
+		Component component = this.wrappedRenderer.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+		if(value instanceof Student)
+			component.setBackground(getTableBackgroundColour((Student) value));
+		return component;
+	}
+}
